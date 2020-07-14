e769f6a9ef2c6187da9e1c65454aca3aec78dbdc
AhmedFathy <ahmed_fathy355@hotmail.com>
2020-07-14 21:26:06 +0200

    dd

--- lib/utility/utils.dart
+++ lib/utility/utils.dart
@@ -2,7 +2,7 @@
 class Utils{
   static String tablevalue = "";
 
-  static String restURL = 'http://195.246.40.141/api/';
+
   //static String RestURL = 'https://localhost:44398/api/';
 
   // DataBadse == Randa
